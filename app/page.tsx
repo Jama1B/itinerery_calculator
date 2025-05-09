8647af383fbc0f104d7c0792792c74c539c6a8f0
Jama1B <84544560+Jama1B@users.noreply.github.com>
2025-05-09 11:43:56 +0300

    connect to db

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { AdminLink } from "@/components/admin-link";
 
 export default function Home() {
   return (
-    <main className="container mx-auto max-w-6xl relative">
+    <main className="container mx-auto max-w-6xl py-6 relative">
       <AdminLink />
       <h1 className="text-3xl font-bold text-center text-green-800 mb-6">
         Safari Itinerary Calculator
